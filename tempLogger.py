00b3f4e95cedb7fec52ea029c9c58d0ec3c876e7
Jonathan L Clark <jonathan84clark@yahoo.com>
2019-12-15 20:49:54 -0800

    Fleshed out the temperature logger.

--- tempLogger.py
+++ tempLogger.py
@@ -7,38 +7,93 @@
 #####################################################################
 # importing the requests library 
 import requests 
+import os.path
+from datetime import date
+from datetime import datetime
+from threading import Thread
+import time
+
+csv_file = "C:\\Users\\jonat\\Desktop\\TempData.txt"
 
 def GetLocalTemp():
     # api-endpoint 
-    URL = "http://192.168.1.177"
+    try:
+        URL = "http://192.168.1.177"
   
-    # sending get request and saving the response as response object 
-    r = requests.get(url = URL) 
+        # sending get request and saving the response as response object 
+        r = requests.get(url = URL) 
   
-    # extracting data in json format 
-    data = r.json() 
-  
+        # extracting data in json format 
+        data = r.json() 
+        return ((data["temperature"] * 9.0/5.0) + 32.0)
 
-    print(data)
+    except Exception as e:
+        print("Exception Get Local Temp: " + str(e))
+        return None
 
 def GetNoaaWeatherData():
     URL1 = "https://api.weather.gov/points/47.688970,-117.283940"
 
-    # sending get request and saving the response as response object 
-    r = requests.get(url = URL1)
+    try:
 
-    # extracting data in json format 
-    data = r.json() 
+        # sending get request and saving the response as response object 
+        r = requests.get(url = URL1)
 
-    forecast_url = data['properties']['forecastHourly']
+        # extracting data in json format 
+        data = r.json() 
+    
+        forecast_url = data['properties']['forecastHourly']
 
-    # sending get request and saving the response as response object 
-    r = requests.get(url = forecast_url)
+        # sending get request and saving the response as response object 
+        r = requests.get(url = forecast_url)
   
-    data = r.json()
+        data = r.json()
 
-    weather = data['properties']['periods'][0]
+        weather = data['properties']['periods'][0]
 
-    print(weather['temperature'])
+        return weather['temperature']
 
-GetNoaaWeatherData()
+    except Exception as e:
+        print("Exception Get Noaa: " + str(e))
+        return None
+
+def WriteToCSV(local_temp, noaa_temp):
+    if not os.path.isfile(csv_file):
+        f = open(csv_file, "a+")
+        f.write("Date,Time,GarageTemp,NoaaTemp\n")
+        f.close()
+    now = datetime.now()
+    dt_string = now.strftime("%m/%d/%Y,%H:%M:%S")
+
+    if local_temp == None:
+        local_tmp_str = "Err"
+    else:
+        local_tmp_str = str(local_temp)
+ 
+    noaa_tmp_str = ""
+    if noaa_temp == None:
+        noaa_tmp_str = "Err"
+    else: 
+        noaa_tmp_str = str(noaa_temp)
+
+    dt_string += "," + local_tmp_str + "," + noaa_tmp_str + "\n"
+    f = open(csv_file, "a+")
+    f.write(dt_string)
+    f.close()
+
+def MainLoop():
+    while (True):
+        noaaTemp = GetNoaaWeatherData()
+        localTemp = GetLocalTemp()
+        print("Wrote temperature data...")
+        WriteToCSV(localTemp, noaaTemp)
+        time.sleep(3600)
+
+if __name__ == "__main__":
+	# Start up the server thread
+    thread = Thread(target = MainLoop)
+    thread.daemon = True
+    thread.start()
+
+    while (True):
+        time.sleep(1)
