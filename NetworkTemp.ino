16e5717eed4389ed65e904a490c6a41597d29882
Jonathan L Clark <jonathan84clark@yahoo.com>
2020-06-20 21:29:58 -0700

    Removed the internet components from the network temperature sensor Added a python driver script to read data from this device from the serial port.

--- NetworkTemp.ino
+++ NetworkTemp.ino
@@ -8,10 +8,10 @@
 * by Tom Igoe
 * Author/Modifier: Jonathan L Clark
 * Date: 12/2/2019
+* Update: 6/20/2020: Removed the code that uses the wifi shield.
 ************************************************************/
 #include <SPI.h>
 #include <Wire.h>
-#include <EthernetV2_0.h>
 #include <Adafruit_Sensor.h>
 #include "SparkFunMPL3115A2.h"
 
@@ -22,17 +22,10 @@
 #define DHTPIN 2
 #define DHTTYPE    DHT11     // DHT 11
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = { 
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192,168,1, 177);
-
 // Initialize the Ethernet server library
 // with the IP address and port you want to use 
 // (port 80 is default for HTTP):
-EthernetServer server(80);
-#define W5200_CS  10
+//EthernetServer server(80);
 #define SDCARD_CS 4
 DHT_Unified dht(DHTPIN, DHTTYPE);
 MPL3115A2 myPressure;
@@ -47,14 +40,8 @@ void setup()
 {
    Wire.begin();        // Join i2c bus
    // Open serial communications and wait for port to open:
-   Serial.begin(9600);
-   pinMode(SDCARD_CS, OUTPUT);
-   digitalWrite(SDCARD_CS,HIGH);//Deselect the SD card
+   Serial.begin(115200);
    // start the Ethernet connection and the server:
-   Ethernet.begin(mac, ip);
-   server.begin();
-   Serial.print("server is at ");
-   Serial.println(Ethernet.localIP());
    // Initialize device.
    dht.begin();
    // Print temperature sensor details.
@@ -101,57 +88,7 @@ void ReadSensor()
 void loop() 
 {
    ms_ticks = millis();
-   // Delay between measurements. (Modified to be non-blocking)
-   if (next_read < ms_ticks)
-   {
-      ReadSensor();
-      next_read = ms_ticks + delayMS;
-   }
-   // listen for incoming clients
-   EthernetClient client = server.available();
-   if (client) 
-   {
-      // an http request ends with a blank line
-      boolean currentLineIsBlank = true;
-      while (client.connected()) 
-      {
-         if (client.available()) 
-         {
-            char c = client.read();
-            // if you've gotten to the end of the line (received a newline
-            // character) and the line is blank, the http request has ended,
-            // so you can send a reply
-            if (c == '\n' && currentLineIsBlank) 
-            {
-               // send a standard http response header
-               client.println("HTTP/1.1 200 OK");
-               client.println("Content-Type: text/json");
-               client.println("Connnection: close");
-               client.println();
-               client.print("{ \"temperature\" : ");
-               client.print(temperature_c);
-               client.print(", \"humidity\" : ");
-               client.print(humidity);
-               client.print(", \"pressure\" : ");
-               client.print(pressure);
-               client.print("}");
-               break;
-            }
-            if (c == '\n') 
-            {
-               // you're starting a new line
-               currentLineIsBlank = true;
-            } 
-            else if (c != '\r') 
-            {
-               // you've gotten a character on the current line
-               currentLineIsBlank = false;
-            }
-         }
-      }
-      // give the web browser time to receive the data
-      delay(1);
-      // close the connection:
-      client.stop();
-   }
+   ReadSensor();
+   Serial.println("Temperature1:" + String(temperature_c) + ":Humidity:" + String(humidity) + ":Pressure:" + String(pressure));
+   delay(500);
 }
---
+++ SerialEnvironment.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#################################################################
+# SERIAL ENVIRONMENT
+# DESC: The serial environment node reads serial data from an environmental
+# sensor.
+# Author: Jonathan L Clark
+# Date: 6/20/2020
+##################################################################
+from threading import Thread
+import json
+import serial
+import re
+
+# Interacts with the Arduino serial sensor
+class EnvironmentalSensor():
+    def __init__(self):
+        self.ser = serial.Serial('COM11')
+        self.ser.baudrate = 115200
+        self.ser.close()
+        self.ser.open()
+
+    # Reads a single data string from the serial port
+    def read(self):
+        line = self.ser.readline()
+        m = re.match(r"\Temperature1:(\d[0-9]+.[0-9]+):Humidity:(\d[0-9]+.[0-9]+):Pressure:(\d[0-9]+.[0-9]+)", line)
+        self.temperature = float(m.group(1))
+        self.humidity = float(m.group(2))
+        self.pressure = float(m.group(3))
+
+environ = EnvironmentalSensor()
+environ.read()
